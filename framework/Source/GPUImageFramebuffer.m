1dc961efdf7b27823b708cc492bd5a267b67585b
burt <sungcheol.kim@hpcnt.com>
2016-03-31 16:01:41 +0900

    - 기존의 comment로 막아 두었던 NSAssert문을 다시 되돌려 놓았습니다.

--- framework/Source/GPUImageFramebuffer.m
+++ framework/Source/GPUImageFramebuffer.m
@@ -272,13 +272,11 @@ void dataProviderUnlockCallback (void *info, const void *data, size_t size);
         return;
     }
 
-//    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
-    if( framebufferReferenceCount > 0) {
-        framebufferReferenceCount--;
-        if (framebufferReferenceCount < 1)
-        {
-            [[GPUImageContext sharedFramebufferCache] returnFramebufferToCache:self];
-        }
+    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
+    framebufferReferenceCount--;
+    if (framebufferReferenceCount < 1)
+    {
+        [[GPUImageContext sharedFramebufferCache] returnFramebufferToCache:self];
     }
 }
 
